0defc3e5a64e5116474bfa852c2e5e2622ea2940
itenyh <itenyh@gmail.com>
2016-05-05 16:59:02 +0800

    xcvx

--- Ex3/lr.py
+++ Ex3/lr.py
@@ -30,19 +30,58 @@ def create_data(size = 1000):
     # print(noisy_data)
     return noisy_data
 
+def linear_model(data):
+
+    X = [[item[0], item[1], 1] for item in data]
+    Y = [item[2]for item in data]
+
+    # X = [[0.5, 0.1, 1], [0.1, 0.2 , 1]]
+    # Y = [[1, -1]]
+
+    X_mat = np.mat(X)
+    Y_mat = np.mat(Y)
+
+    H = X_mat * (X_mat.T * X_mat).I * X_mat.T
+    Y_p = H * Y_mat.T
+    sum_error = ((Y_mat - Y_p.T) * (Y_mat - Y_p.T).T) / 1000
+
+
+# linear_model(create_data())
+
+
 data = create_data()
+# data = [[0.5, 0.1, 1], [0.1, 0.2 , -1]]
 X = [[item[0], item[1], 1] for item in data]
 Y = [item[2]for item in data]
-
-# X = [[0.5, 0.1, 1], [0.1, 0.2 , 1]]
-# Y = [[1, -1]]
 
 X_mat = np.mat(X)
 Y_mat = np.mat(Y)
 
-H = X_mat * (X_mat.T * X_mat).I * X_mat.T
-Y_p = H * Y_mat.T
+X1 = [[item[0]] for item in data]
+X2 = [[item[1]] for item in data]
+X1X2 = [[x1[0] * x2[0]] for x1, x2 in zip(X1, X2)]
+X1p = [[x1[0] * x1[0]] for x1 in X1]
+X2p = [[x2[0] * x2[0]] for x2 in X2]
+
+new_X = []
+for i in range(0, len(X1)):
+
+    new_X.append([X1[i][0], X2[i][0], X1X2[i][0], X1p[i][0], X2p[i][0], 1])
+
+X_mat = np.mat(new_X)
+w1 = [-0.05, 0.08, 0.13, 1.5, 15, -1] #36.814
+w2 = [-1.5, 0.08, 0.13, 0.05, 1.5, -1] #1.85
+w3 = [-1.5, 0.08, 0.13, 0.05, 0.05, -1] #2.7222
+w4 = [-1.5, 0.05, 0.13, 1.5, 1.5, -1] #1.31
+w5 = [-1.5, 0.05, 0.13, 15, 1.5, -1] #39.42
+w_p = [0, 0, 0, 1, 1, -0.6] #39.42
+
+W = np.mat(w_p).T
+
+Y_p = X_mat * W
 sum_error = ((Y_mat - Y_p.T) * (Y_mat - Y_p.T).T) / 1000
+print(sum_error)
+
 
 
 
